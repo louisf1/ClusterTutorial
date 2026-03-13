48c2533833187fd20e8b15382565ceb1e02b1145
Louis Faasen <louisfaasen@gmail.com>
2026-03-13 16:18:19 +0200

    Fix: Resolve Wayland interface linkage and enable C support in CMake

--- start_dash.sh
+++ start_dash.sh
@@ -1,13 +1,14 @@
 #/bin/sh
 
+# 1. Clean up old build artifacts
 cd /home/louis/Documents/QtDesignStudio/examples/ClusterWorx/build
-##export QT_QPA_PLATFORM=wayland-egl
-# 1. Link the system libraries
-
-# 2. Set the environment (Order of paths is CRITICAL)
-export QML_IMPORT_PATH=/home/louis/Documents/QtDesignStudio/examples/ClusterWorx/imports
-export LD_LIBRARY_PATH=/home/louis/Documents/QtDesignStudio/examples/ClusterWorx/custom_libs:/home/louis/Qt/6.10.2/gcc_64/lib:$LD_LIBRARY_PATH
-
+## 2. Set the environment (REVERSED PRIORITY)
+# 2. Set the environment (Order is VITAL)
+# We must point to the Qt 6.10.2 system QML folder for Qt5Compat and Studio Effects
+export QT_DIR=/home/louis/Qt/6.10.2/gcc_64
+export QML2_IMPORT_PATH=$QT_DIR/qml
+export QML_IMPORT_PATH=/home/louis/Documents/QtDesignStudio/examples/ClusterWorx/imports:$QML2_IMPORT_PATH
+export LD_LIBRARY_PATH=$QT_DIR/lib:/home/louis/Documents/QtDesignStudio/examples/ClusterWorx/custom_libs:$LD_LIBRARY_PATH
+cp ../VSS_paths.txt .
 # 3. Run directly from the build folder
 ./ClusterDashboardApp
-
